a6003a0299c2a02cab9c3a81d249d64c29348d57
Gabriele Angeline <adgabriele@gmail.com>
2014-04-06 18:55:26 -0700

    Initial Commit

---
+++ homework5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  homework5
+//
+//  Created by Gabriele Angeline on 4/6/14.
+//  Copyright (c) 2014 Gabriele Angeline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
